07da1a9c4f1a43cf7df9edabcc5588ee158faefc
Kevin22888 <kevin.jx.yang@gmail.com>
2025-06-21 16:06:34 -0400

    Update project hierarchy

--- include/Particle.h
+++ include/Particle.h
@@ -1,20 +1,33 @@
-#ifndef PBF_PARTICLE_H
-#define PBF_PARTICLE_H
+#ifndef AQUAFORGE_PARTICLE_H
+#define AQUAFORGE_PARTICLE_H
 
 #include <glm/glm.hpp>
 
+namespace AquaForge {
+
 struct Particle {
+    // Primary physical states
     glm::vec3 position;
     glm::vec3 velocity;
     glm::vec3 new_position;
     glm::vec3 new_velocity;
+    
+    // Density Solver adjustment
     glm::vec3 position_delta;
+
+    // SPH fields
     float mass;
     float density;
-    uint32_t hash;
     float lambda;
+    
+    // Spatial hashing
+    uint32_t hash;
+    
+    // Rendering
     glm::vec4 color;
     float size;
 };
 
-#endif // PBF_PARTICLE_H
+} // namespace AquaForge
+
+#endif // AQUAFORGE_PARTICLE_H
